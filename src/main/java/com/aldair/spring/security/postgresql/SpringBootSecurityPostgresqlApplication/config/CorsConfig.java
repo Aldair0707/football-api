47129918bb9be5aee3dce0d2999a71a443db7245
Aldair0707 <aldairhernandez07124@gmail.com>
2025-06-13 01:43:13 -0600

    second commit

---
+++ src/main/java/com/aldair/spring/security/postgresql/SpringBootSecurityPostgresqlApplication/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.aldair.spring.security.postgresql.SpringBootSecurityPostgresqlApplication.config;
+/* 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("http://localhost:4200") // ← ¡AQUÍ! NO uses "*" si allowCredentials está en true
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+*/
--- src/main/java/com/aldair/spring/security/postgresql/SpringBootSecurityPostgresqlApplication/controllers/ReaccionController.java
+++ src/main/java/com/aldair/spring/security/postgresql/SpringBootSecurityPostgresqlApplication/controllers/ReaccionController.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/reacciones")
-@CrossOrigin(origins = "*", maxAge = 3600)
+//@CrossOrigin(origins = "*", maxAge = 3600)
 public class ReaccionController {
 
     @Autowired
