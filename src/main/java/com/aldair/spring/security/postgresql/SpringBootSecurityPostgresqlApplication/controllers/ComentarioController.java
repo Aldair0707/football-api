e46b04336fbdc459f204bd2bdc21cfc5d1078d42
Aldair0707 <aldairhernandez07124@gmail.com>
2025-06-13 01:43:13 -0600

    second commit

--- src/main/java/com/aldair/spring/security/postgresql/SpringBootSecurityPostgresqlApplication/controllers/ComentarioController.java
+++ src/main/java/com/aldair/spring/security/postgresql/SpringBootSecurityPostgresqlApplication/controllers/ComentarioController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+//@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/comentarios")
 public class ComentarioController {
--- src/main/java/com/aldair/spring/security/postgresql/SpringBootSecurityPostgresqlApplication/security/WebSecurityConfig.java
+++ src/main/java/com/aldair/spring/security/postgresql/SpringBootSecurityPostgresqlApplication/security/WebSecurityConfig.java
@@ -1,21 +1,24 @@
 package com.aldair.spring.security.postgresql.SpringBootSecurityPostgresqlApplication.security;
 
-
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.aldair.spring.security.postgresql.SpringBootSecurityPostgresqlApplication.security.jwt.AuthEntryPointJwt;
 import com.aldair.spring.security.postgresql.SpringBootSecurityPostgresqlApplication.security.jwt.AuthTokenFilter;
@@ -23,35 +26,27 @@ import com.aldair.spring.security.postgresql.SpringBootSecurityPostgresqlApplica
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
-// (securedEnabled = true,
-// jsr250Enabled = true,
-// prePostEnabled = true) // by default
 public class WebSecurityConfig {
+
   @Autowired
   UserDetailsServiceImpl userDetailsService;
 
   @Autowired
   private AuthEntryPointJwt unauthorizedHandler;
 
-  //@Autowired
-  //private AuthTokenFilter authTokenFilter;
-
-
   @Bean
   public AuthTokenFilter authenticationJwtTokenFilter() {
-   return new AuthTokenFilter();
+    return new AuthTokenFilter();
   }
 
   @Bean
   public DaoAuthenticationProvider authenticationProvider() {
-      DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-       
-      authProvider.setUserDetailsService(userDetailsService);
-      authProvider.setPasswordEncoder(passwordEncoder());
-   
-      return authProvider;
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
   }
- 
+
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
     return authConfig.getAuthenticationManager();
@@ -61,26 +56,56 @@ public class WebSecurityConfig {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
   
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-   http.csrf(csrf -> csrf.disable())
-       .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
-       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-       .authorizeHttpRequests(auth ->
-         auth.requestMatchers("/api/auth/**").permitAll()
-             .requestMatchers("/api/test/**").permitAll()
-             .requestMatchers("/api/publicaciones/**").authenticated()
-             .requestMatchers("/api/publicaciones").authenticated()
-             .requestMatchers("/api/comentarios/**").authenticated()
-             .requestMatchers("/api/reacciones/**").authenticated()
-             .anyRequest().denyAll()
-       );
-  
-   http.authenticationProvider(authenticationProvider());
-   http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    http
+      .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+      .csrf(csrf -> csrf.disable())
+      .exceptionHandling(ex -> ex.authenticationEntryPoint(unauthorizedHandler))
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .authorizeHttpRequests(auth -> auth
+        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() 
+        .requestMatchers("/api/auth/**").permitAll()
+        .requestMatchers("/api/test/**").permitAll()
+        .requestMatchers("/api/publicaciones/**").permitAll()
+        .requestMatchers("/api/comentarios/**").permitAll()
+        .requestMatchers("/api/reacciones/**").permitAll()
+        .anyRequest().authenticated()
+      ); 
+  /*@Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> 
+          auth.requestMatchers("/api/auth/**").permitAll()
+              .requestMatchers("/api/test/**").permitAll()
+              .requestMatchers("/api/publicaciones/**").permitAll()
+              .requestMatchers("/api/reacciones/**").permitAll()
+              .requestMatchers("/api/comentarios/**").permitAll()
+              .anyRequest().authenticated()
+        );*/
 
-    
+    http.authenticationProvider(authenticationProvider());
+    http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
     return http.build();
   }
+  
+  
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of("http://localhost:4200")); 
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
+  
 }
